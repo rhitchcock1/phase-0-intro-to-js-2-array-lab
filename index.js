54e1d530c807d6a656fe6bfa428d3f271f6157b6
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-01-02 19:42:11 -0500

    commit tree a3866f6fda61757ca976633d3217e7d49e8e2a67

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){cats.push('Ralph');}
+function destructivelyPrependCat(){cats.unshift('Bob');}
+function destructivelyRemoveLastCat(){cats.pop('Ralp');}
+function destructivelyRemoveFirstCat(){cats.shift('Bob"');}
+function appendCat (){const copyofCats =[...cats,"Broom"]; return copyofCats}
+function prependCat (){const copyofCats =["Arnold", ...cats];return copyofCats}
+function removeLastCat(){const newCats = cats.slice(2); return newCats}
+function removeFirstCat (){const newNewCats= cats.slice(0);return newNewCats}
