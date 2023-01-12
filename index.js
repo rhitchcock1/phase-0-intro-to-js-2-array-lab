9db0e01c4986b1c387fffb9ce31a037c85bd50bb
Robert Hitchcock <robert.e.hitchcock@gmail.com>
2023-01-12 09:51:50 -0500

    arryas lab phase-1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){cats.push('Ralph');}
+function destructivelyPrependCat(){cats.unshift('Bob');}
+function destructivelyRemoveLastCat(){cats.pop('Ralp');}
+function destructivelyRemoveFirstCat(){cats.shift('Bob"');}
+function appendCat (){const copyofCats =[...cats,"Broom"]; return copyofCats}
+function prependCat (){const copyofCats =["Arnold", ...cats];return copyofCats}
+function removeLastCat(){
+    return cats.slice(0, cats.length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
